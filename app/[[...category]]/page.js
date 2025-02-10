1e1feb0b289f2ad2b5badc0e3cb2270f0461def1
EyupCanbay <eyupcanbay40@gmail.com>
2025-02-10 18:40:56 +0300

    "Add fetching data functions "

--- app/[[...category]]/page.js
+++ app/[[...category]]/page.js
@@ -2,19 +2,80 @@ import React from "react";
 import HomeContainer from "@/containers/home/index.js";
 import Movies from "@/mocks/movie.json"
 
-async function delay(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms))
+const API_URL = "https://api.themoviedb.org/3"
+
+const getPopularMovies = async () => {
+  const res = await fetch(`https://api.themoviedb.org/3/movie/popular`,
+    {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.API_TOKEN}` 
+      }
+    }
+  )
+  return res.json()
+}
+
+const getFavMovies = async() => {
+  const res = await fetch(`${API_URL}/movie/top_rated?language=en-US&page=1`,
+    {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.API_TOKEN}` 
+      }
+    }
+  )
+  return res.json()
+}
+
+const getGenresFetch = async() => {
+  const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?language=en',
+    {method: 'GET',
+      headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${process.env.API_TOKEN}` 
+      }
+    }
+  )
+  return res.json();
+}
+
+const getSingleCategory = async() => {
+  const res = await fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc',
+  {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${process.env.API_TOKEN}` 
+    }
+  }
+)
+return res.json()
+
 }
 
 async function Home({ params }) {
-
-  await delay(5000);
   let selectedCategory
+
+  const topRatedPromise = getPopularMovies()
+  const favMoviesPromise = getFavMovies()
+  const genresPromise = getGenresFetch()
+
+const [{results: popularMovies}, {results: favMovies}, {genres: categories} ] =
+  await Promise.all([topRatedPromise, favMoviesPromise, genresPromise]);
+  
   if(params.category?.length > 0){
-    selectedCategory = true;
+    const { results } = await getSingleCategory(params.category[0]);
+    selectedCategory = results;
   }
+
   return (
     <HomeContainer 
+      popularMovies={popularMovies}
+      favMovies={favMovies}
+      categories={categories}
       selectedCategory = {{
         id: params.category?.[0] ?? "",
         movies : selectedCategory ? Movies.results.slice(0, 6) : []
--- containers/home/index.js
+++ containers/home/index.js
@@ -1,19 +1,18 @@
 import React from "react";
-import Movies from "@/mocks/movie.json"
-import Genres from "@/mocks/genres.json"
 import { FeaturedMovie } from "@/components/featured-movie";
 import Categories from "@/components/categories";
 import MovieSection from "@/components/movie-section";
 
-function HomeContainer({ selectedCategory }) {
-    return (
+
+function HomeContainer({ popularMovies = [], favMovies=[], categories=[], selectedCategory }) {
+  return (
       <div>
-        <FeaturedMovie movie={Movies.results[0]}/>
-        <Categories categories={Genres.genres.slice(0, 5)}/>
+        <FeaturedMovie movie={popularMovies?.[0]}/>
+        <Categories categories={categories.slice(0, 5)}/>
         
         {(selectedCategory.movies.length > 0) && (
           <MovieSection 
-            title={Genres.genres.find(
+            title={categories.find(
               (genre)=> genre.id.toString() === selectedCategory.id.toString()
             ).name}
             movies={selectedCategory.movies}
@@ -21,11 +20,11 @@ function HomeContainer({ selectedCategory }) {
         )}
         <MovieSection 
         title="Popular Films"
-        movies={Movies.results.slice(1, 7)}
+        movies={popularMovies.slice(1, 7)}
         />
         <MovieSection 
         title="Best Films"
-        movies={Movies.results.slice(7, 13)}
+        movies={favMovies.slice(7, 13)}
         />
       </div>
   );
